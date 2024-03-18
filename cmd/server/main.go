ce8118e2eb7b7eda1ff42a585ce8254b72e132d1
mohdjishin <whoistcp@protonmail.com>
2024-03-19 01:56:37 +0530

    restructured

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -2,72 +2,27 @@ package main
 
 import (
 	"context"
-	"crypto/rsa"
-	"crypto/tls"
-	"crypto/x509"
-	"encoding/pem"
 	"log"
 	"net"
-	"os"
 
 	pb "github.com/mohdjishin/gRPC-Secure/internal/api/greeterService"
-	util "github.com/mohdjishin/gRPC-Secure/pkg/utils"
+	"github.com/mohdjishin/gRPC-Secure/pkg/utils"
 	"google.golang.org/grpc"
-	"google.golang.org/grpc/credentials"
 )
 
-// GreeterServer implements the greeter service
 type GreeterServer struct {
 	pb.UnimplementedGreeterServer
 }
 
-// SayHello implements the SayHello method of the Greeter service
 func (s *GreeterServer) SayHello(ctx context.Context, in *pb.HelloRequest) (*pb.HelloReply, error) {
 	return &pb.HelloReply{Message: "Hello, " + in.Name + "!"}, nil
 }
 
 func main() {
-	// Load server certificate and key
-	p12File, err := os.ReadFile("certs/server/server.p12")
-	if err != nil {
-		log.Fatalf("failed to read P12 file: %v", err)
-	}
-	privKey, leafCert, caCert, err := util.DecodeP12File(p12File, "yourpassword")
-	if err != nil {
-		log.Fatalf("failed to decode P12 file: %v", err)
-	}
+	grpcServer := grpc.NewServer(grpc.Creds(utils.LoadTLSCredentials()))
 
-	// Convert private key to PEM format
-	privKeyPEM := pem.EncodeToMemory(&pem.Block{
-		Type:  "RSA PRIVATE KEY",
-		Bytes: x509.MarshalPKCS1PrivateKey(privKey.(*rsa.PrivateKey)),
-	})
-
-	// Convert leaf certificate to PEM format
-	leafCertPEM := pem.EncodeToMemory(&pem.Block{
-		Type:  "CERTIFICATE",
-		Bytes: leafCert.Raw,
-	})
-
-	// Create TLS certificate from private key and leaf certificate
-	serverCert, err := tls.X509KeyPair(leafCertPEM, privKeyPEM)
-	if err != nil {
-		log.Fatalf("failed to create TLS certificate: %v", err)
-	}
-
-	// Create TLS credentials
-	creds := credentials.NewTLS(&tls.Config{
-		Certificates: []tls.Certificate{serverCert},
-		RootCAs:      caCert,
-	})
-
-	// Create a gRPC server with TLS credentials
-	grpcServer := grpc.NewServer(grpc.Creds(creds))
-
-	// Register the Greeter service
 	pb.RegisterGreeterServer(grpcServer, &GreeterServer{})
 
-	// Start the server
 	lis, err := net.Listen("tcp", ":50051")
 	if err != nil {
 		log.Fatalf("failed to listen: %v", err)
--- pkg/utils/utils.go
+++ pkg/utils/utils.go
@@ -1,12 +1,18 @@
 package utils
 
 import (
+	"crypto/rsa"
+	"crypto/tls"
 	"crypto/x509"
+	"encoding/pem"
+	"log"
+	"os"
 
+	"google.golang.org/grpc/credentials"
 	"software.sslmate.com/src/go-pkcs12"
 )
 
-func DecodeP12File(p12Data []byte, password string) (privateKey interface{}, leaf *x509.Certificate, roots *x509.CertPool, err error) {
+func decodeP12File(p12Data []byte, password string) (privateKey interface{}, leaf *x509.Certificate, roots *x509.CertPool, err error) {
 
 	privateKey, certificate, ca, err := pkcs12.DecodeChain(p12Data, password)
 	if err != nil {
@@ -22,4 +28,81 @@ func DecodeP12File(p12Data []byte, password string) (privateKey interface{}, lea
 	}
 
 	return
+}
+
+func CreateClientCredentials(p12FilePath, password string) (credentials.TransportCredentials, error) {
+	// Load client certificate and key
+	p12Data, err := os.ReadFile(p12FilePath)
+	if err != nil {
+		return nil, err
+	}
+
+	privKey, leafCert, caCert, err := decodeP12File(p12Data, password)
+	if err != nil {
+		return nil, err
+	}
+
+	// Convert private key to PEM format
+	privKeyPEM := pem.EncodeToMemory(&pem.Block{
+		Type:  "RSA PRIVATE KEY",
+		Bytes: x509.MarshalPKCS1PrivateKey(privKey.(*rsa.PrivateKey)),
+	})
+
+	// Convert leaf certificate to PEM format
+	leafCertPEM := pem.EncodeToMemory(&pem.Block{
+		Type:  "CERTIFICATE",
+		Bytes: leafCert.Raw,
+	})
+
+	// Create TLS certificate from leaf certificate and private key
+	clientCert, err := tls.X509KeyPair(leafCertPEM, privKeyPEM)
+	if err != nil {
+		return nil, err
+	}
+
+	// Create TLS credentials
+	creds := credentials.NewTLS(&tls.Config{
+		Certificates: []tls.Certificate{clientCert},
+		RootCAs:      caCert,
+	})
+
+	return creds, nil
+}
+
+func LoadTLSCredentials() credentials.TransportCredentials {
+	// Load server certificate and key
+	p12File, err := os.ReadFile("certs/server/server.p12")
+	if err != nil {
+		log.Fatalf("failed to read P12 file: %v", err)
+	}
+	privKey, leafCert, caCert, err := decodeP12File(p12File, "yourpassword")
+	if err != nil {
+		log.Fatalf("failed to decode P12 file: %v", err)
+	}
+
+	// Convert private key to PEM format
+	privKeyPEM := pem.EncodeToMemory(&pem.Block{
+		Type:  "RSA PRIVATE KEY",
+		Bytes: x509.MarshalPKCS1PrivateKey(privKey.(*rsa.PrivateKey)),
+	})
+
+	// Convert leaf certificate to PEM format
+	leafCertPEM := pem.EncodeToMemory(&pem.Block{
+		Type:  "CERTIFICATE",
+		Bytes: leafCert.Raw,
+	})
+
+	// Create TLS certificate from private key and leaf certificate
+	serverCert, err := tls.X509KeyPair(leafCertPEM, privKeyPEM)
+	if err != nil {
+		log.Fatalf("failed to create TLS certificate: %v", err)
+	}
+
+	// Create TLS credentials
+	creds := credentials.NewTLS(&tls.Config{
+		Certificates: []tls.Certificate{serverCert},
+		RootCAs:      caCert,
+	})
+
+	return creds
 }
